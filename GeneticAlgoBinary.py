0c269080980df2f2c1cc4e6430c70994d93940d9
Brandon Da Silva <brandinho@Brandons-MBP.home>
2018-10-17 23:08:09 -0400

    Updated Files

--- GeneticAlgoBinary.py
+++ GeneticAlgoBinary.py
@@ -15,7 +15,7 @@ def crossover(parent_1, parent_2, crossover_probability, population_size, crosso
     # We can choose to randomly select which crossover type to use
     # If this is set to False, then we have to select one of the options in our function inputs
     if random_type:
-        crossover_type = np.random.choice(np.array["One Point", "Two Point", "Uniform", "Arithmetic"])
+        crossover_type = np.random.choice(np.array["One Point", "Two Point", "Uniform"])
     which_parent = np.random.choice(np.array(["Parent 1", "Parent 2"]))
     if crossover_flag == True:
         if crossover_type == "One Point":
@@ -50,10 +50,6 @@ def crossover(parent_1, parent_2, crossover_probability, population_size, crosso
             elif which_parent == "Parent 2":
                 child = parent_2
                 child[np.sort(random_sequence)] = parent_1[np.sort(random_sequence)]
-        elif crossover_type == "Arithmetic":
-            # Rather than swap genes to form a new chromosome, we are doing some arithmetics to make a new chromosome
-            random_weight = np.random.rand()
-            child = parent_1 * random_weight + parent_2 * (1 - random_weight)
         else:
             raise ValueError("Sorry, that is not one of the options")        
     else:
@@ -68,66 +64,46 @@ def crossover(parent_1, parent_2, crossover_probability, population_size, crosso
 
 
 # Mutate a portion of the gene
-def mutation(population, mutate_probability, population_size, noise_scale):
-    # We start at position 2 because we don't want to mutate the two parents that we are carrying forward
-    # This is called elitism, and we do it to always preserve the best chromosomes through the generations
+def mutation(population, mutate_probability, population_size, mutation_type, random_type = False):
     for p in range(2, population_size):            
         child = population[p,]
         mutate_flag = np.random.random() < mutate_probability
         if mutate_flag == True:
-            # noise_scale determines how much noise we want to use in the mutation (the variance of the distribution)
-            noise = np.random.standard_normal() * noise_scale
-            mutation_position = np.random.randint(0, population.shape[1])
-            child[mutation_position] = child[mutation_position] + noise
-            if child[1] < child[0]:
-                child[1] = child[0] + 1
+            if random_type:
+                mutation_type = np.random.choice(np.array["Flip", "Swap", "Inversion", "Scramble"])
+            
+            if mutation_type == "Flip":
+                # This mutation selects a random position and flips the binary encoding from 0 to 1 and vice versa
+                random_position = np.random.randint(0, child.shape[0])
+                if child[random_position] == 1:
+                    child[random_position] = 0
+                else:
+                    child[random_position] = 1
+            elif mutation_type == "Swap":
+                # This mutation selects two random positions in the chromosome and swaps their genes (values)
+                random_positions = np.random.choice(np.arange(child.shape[0]), 2, replace = False)
+                value_1, value_2 = child[random_positions[0]], child[random_positions[1]]
+                child[random_positions[0]], child[random_positions[1]] = value_2, value_1
+            elif mutation_type == "Inversion":
+                # This mutation selects two random positions and inverts/reverses the substring of genes between them
+                lower_limit = np.random.randint(0, child.shape[0]-1)
+                upper_limit = np.random.randint(lower_limit+1, child.shape[0])
+                child[lower_limit:upper_limit+1] = child[lower_limit:upper_limit+1][::-1]
+            elif mutation_type == "Scramble":
+                # This mutation selects two random positions, and scrambles the positions genes within them
+                lower_limit = np.random.randint(0, child.shape[0]-1)
+                upper_limit = np.random.randint(lower_limit+1, child.shape[0])
+                scrambled_order = np.random.choice(np.arange(lower_limit, upper_limit+1), upper_limit + 1 - lower_limit, replace = False)
+                child[lower_limit:upper_limit+1] = child[scrambled_order]
+            else:
+                raise ValueError("Sorry, that is not one of the options") 
+                
         population[p,] = child
     return population
 
 
 
-# Generate a new population with gaussian noise
-def generate_gaussian_population(population, competition_scores, elite_population, selection_method, 
-                                 population_size, feature_size):
-    new_population = np.zeros((population_size, feature_size))
-    # We are using the concept of elitism, so we keep the top two performers from the previous generation
-    new_population[:2,] = elite_population
-    # We also want to make sure to use them each at least once in the "breeding" process
-    new_population[2:4,] = elite_population
-        
-    if any(np.array(competition_scores) < 0): competition_scores += -min(competition_scores)
-    population_index_sorted = np.argsort(competition_scores)[::-1]
-    competition_scores_sorted = np.sort(competition_scores)[::-1]
-    scores_cumulsum = np.cumsum(competition_scores_sorted)
-    for p in range(2, population_size):
-        if p > 3:
-            if selection_method == "Roulette":
-                random_number = np.random.uniform(low = 0, high = scores_cumulsum[-1])
-                if all(scores_cumulsum == scores_cumulsum[0]):
-                    position = 0
-                else:
-                    # Randomly select the parent with a probability proportional to their competition score
-                    position = next(x[0] for x in enumerate(scores_cumulsum) if x[1] > random_number)
-                parent = population[population_index_sorted[position],]
-            elif selection_method == "Tournament":
-                # Host a mini tournament to see who will become the parent that we add gaussian noise to
-                k = population_size // 2
-                tournament_population = np.zeros((k, 1))
-                total_competitors = np.random.choice(np.arange(population_size), k, replace = False)
-                tournament_population[:,0] = competition_scores[total_competitors]
-                
-                parent_index = total_competitors[np.argmax(tournament_population, axis = 0)]
-                parent = population[parent_index,]
-        else:
-            parent = population[p]
-        
-        new_population[p,] = parent + np.random.standard_normal(parent.shape[0])
-
-    return new_population
-
-
-
-# Generate a new population with crossover and mutation
+# Generate a new population
 def generate_population(population, competition_scores, elite_population, selection_method, crossover_probability, 
                         crossover_type, random_type, population_size, feature_size):
     new_population = np.zeros((population_size, feature_size))
@@ -169,34 +145,35 @@ def generate_population(population, competition_scores, elite_population, select
 
 
 # Run the genetic algorithm
-def genetic_algorithm(run_competition_function, neural_network_shell, num_generations, population_size, gaussian_or_crossmutate, 
+def genetic_algorithm(neural_network_shell, num_generations, population_size, gaussian_or_crossmutate, 
                       crossover_probability, crossover_type, random_type, mutate_probability, noise_scale, selection_method, 
                       neurons, *args, **kwargs):
     # This array will keep track of the best scores for each generation
     best_scores = np.zeros(num_generations)
 
     # The specific example I'm showing below is for a reinforcement learning agent that is a parameterized neural network
-    # We are using a genetic algorithm to learn the weights of the neural network - continuous variable optimization
+    # We are using a genetic algorithm to learn the optimal set of features to use for our model (feature selection)
     tf.reset_default_graph()
-    sess = tf.Session()
-    agent = neural_network_shell(sess, neurons, population_size)
-    sess.run(tf.global_variables_initializer())
+    agent = neural_network_shell(neurons, population_size)
     
     for gen in range(num_generations):
 
         # You can define whatever competition function that you like and replace the one below
         # You can also put in actual function arguments instead of *args and **kwargs
-        # We are assuming you have initialized your agent class with a population size attribute
-        competition_scores = np.array([run_competition_function(*args, **kwargs) for agent_index in range(agent.population_size)])
+        # We are assuming you have initialized your agent class with a population and population size attribute
+        # The population attribute is an array consisting of binary encoded vectors (each row is a different binary encoding)
+        # indicating which features to use
+        competition_scores = np.array([agent.initialize_and_train(inputs = agent.population[agent_index,]) for agent_index in range(agent.population_size)])
 
         best_scores[gen] = np.max(competition_scores)
         # We are assuming you initialized your agent class with a numpy array (population attribute) 
         # to store chromosomes (i.e. neural network weights)
         elite_population = agent.population[competition_scores.argsort()[-2:][::-1],]
-        if gaussian_or_crossmutate == "Gaussian":
-            agent.population = generate_gaussian_population(agent.population, competition_scores, elite_population, selection_method, population_size, agent.population.shape[1])
-        elif gaussian_or_crossmutate == "Crossmutate":
-            agent.population = generate_population(agent.population, competition_scores, elite_population, selection_method, 
-                                                   crossover_probability, crossover_type, random_type, population_size, agent.population.shape[1])
-            agent.population = mutation(agent.population, mutate_probability, population_size, noise_scale)
+
+        agent.population = generate_population(agent.population, competition_scores, elite_population, selection_method, 
+                                               crossover_probability, crossover_type, random_type, population_size, agent.population.shape[1])
+        agent.population = mutation(agent.population, mutate_probability, population_size, noise_scale)
+    
+    # We are returning the elite population (which are the top two binary encodings)    
+    return elite_population
 
